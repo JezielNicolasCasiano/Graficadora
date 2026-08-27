790654102d8a0b0d24221c23b618f20fdf97f609
aaronnd01 <aaron.fuentes0107@gmail.com>
2026-08-27 00:05:29 -0600

    Punto2D y Vector2D ya estan completos, en Punto3D me faltan unos metodos y preparar Vector3D

--- src/main/java/jeziel/graficadora/Modelos/Punto2D.java
+++ src/main/java/jeziel/graficadora/Modelos/Punto2D.java
@@ -1,5 +1,41 @@
 package jeziel.graficadora.Modelos;
 
-public class Punto2D extends Vector{
+public class Punto2D extends Punto{
+float ordenadaX;
+float ordenadaY;
 
+    public Punto2D(float x, float ordenadaX) {
+        this.ordenadaX = ordenadaX;
+    }
+
+    public float getOrdenadaY() {
+        return ordenadaY;
+    }
+
+    public void setOrdenadaY(float ordenadaY) {
+        this.ordenadaY = ordenadaY;
+    }
+
+    public void setOrdenadaX(float ordenadaX) {
+        this.ordenadaX = ordenadaX;
+    }
+
+    @Override
+    public float obtenerDistanciaPunto(Punto otro) {
+        if(otro instanceof Punto2D){
+            Punto2D p = (Punto2D) otro;
+            float dx = this.ordenadaX - p.ordenadaX;
+            float dy = this.ordenadaY - p.ordenadaY;
+            return (float) Math.sqrt(dx*dx + dy*dy);
+        }
+        return 0;
+    }
+    public Vector2D vectorHastaPunto(Punto2D p){
+        return new Vector2D(p.ordenadaX - this.ordenadaX,p.ordenadaY - this.ordenadaY);
+    }
+    public Punto2D trasladarPunto(Vector2D v){
+
+        return new Punto2D(this.ordenadaX + v.getVectorX(), this.ordenadaY + v.getVectorY());
+
+    }
 }
--- src/main/java/jeziel/graficadora/Modelos/Vector2D.java
+++ src/main/java/jeziel/graficadora/Modelos/Vector2D.java
@@ -1,4 +1,52 @@
 package jeziel.graficadora.Modelos;
 
-public class Vector2D {
+public class Vector2D extends Vector{
+    float vectorX;
+    float vectorY;
+    Float magnitud;
+
+    public Vector2D(float vectorX, Float vectorY) {
+        this.vectorX = vectorX;
+        this.vectorY = vectorY;
+        this.magnitud = (float) Math.sqrt(vectorX*vectorX + vectorY * vectorY);
+    }
+
+    public float getVectorX() {
+        return vectorX;
+    }
+
+    public void setVectorX(float vectorX) {
+        this.vectorX = vectorX;
+    }
+
+    public float getVectorY() {
+        return vectorY;
+    }
+
+    public void setVectorY(float vectorY) {
+        this.vectorY = vectorY;
+    }
+    @Override
+    public float obtenerMagnitud(){
+        return this.magnitud;
+    }
+    public Vector2D sumar(Vector2D v){
+
+        return new Vector2D(this.vectorX + v.vectorX, this.vectorY + v.vectorY);
+    }
+    public Vector2D restar(Vector2D v){
+
+        return new Vector2D(this.vectorX -v.vectorX, this.vectorY - v.vectorY);
+
+    }
+    public Vector2D multEscalar(float k){
+
+        return new Vector2D(this.vectorX*k,this.vectorY*k);
+
+    }
+    public float obtenerProductoPunto(Vector2D v){
+
+        return (this.vectorX * v.vectorX) + (this.vectorY * v.vectorY);
+
+    }
 }
